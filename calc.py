c92295f89e00d0fa504398fa8d0b80b6f0eed87c
George Aptsiauri <George.Aptsiauri@gmail.com>
2024-11-16 16:03:46 +0400

    Add mult and if operation for 0

--- calc.py
+++ calc.py
@@ -2,9 +2,11 @@ print("The best calculator ever")
 x = int(input("X: "))
 y = int(input("Y: "))
 
-print(f" SUM: {x + y}")
+print(f"SUM: {x + y}")
+print(f"SUB: {x - y}")
+print(f"MUL: {x * y}")
 
-
-print(f" SUB: {x - y}")
-
-
+if y == 0:
+	print("Can't do it!")
+else:
+	print(f"Div: {x / y}")
